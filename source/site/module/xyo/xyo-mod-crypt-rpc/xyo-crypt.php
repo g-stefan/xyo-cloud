d81b3d996c972c81f331c469ae590340f4e21821
Grigore Stefan <g_stefan@yahoo.com>
2021-01-02 18:10:21 +0200

    copyright

--- source/site/module/xyo/xyo-mod-crypt-rpc/xyo-crypt.php
+++ source/site/module/xyo/xyo-mod-crypt-rpc/xyo-crypt.php
@@ -2,7 +2,7 @@
 //
 // XYO Crypt Library
 //
-// Copyright (c) 2020 Grigore Stefan <g_stefan@yahoo.com>
+// Copyright (c) 2020-2021 Grigore Stefan <g_stefan@yahoo.com>
 // Created by Grigore Stefan <g_stefan@yahoo.com>
 //
 // MIT License (MIT) <http://opensource.org/licenses/MIT>
