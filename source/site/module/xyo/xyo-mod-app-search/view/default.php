722e7ae22658c7c9036f1b4fe68281f4016226c4
Grigore Stefan <g_stefan@yahoo.com>
2021-04-11 19:46:15 +0300

    search

--- source/site/module/xyo/xyo-mod-app-search/view/default.php
+++ source/site/module/xyo/xyo-mod-app-search/view/default.php
@@ -11,14 +11,16 @@ defined("XYO_CLOUD") or die("Access is denied");
 if($this->cloud->hasApplication()) {
 	$modApp=&$this->cloud->getModule($this->cloud->getApplication());
 	if($modApp instanceof xyo_mod_Application) {
-		if($modApp->hasSearch()){
-			echo "<form name=\"application_search\" id=\"form_application_search\">";
-				echo "<div class=\"xui form-input-group\">";
-				echo "<input type=\"text\" name=\"search\" value=\"\" size=\"32\" placeholder=\"Search\" id=\"application_search\">";
-				echo "<button type=\"submit\" name=\"submit_search\" onclick=\"XYO.Application.doSearch();return false;\"><i class=\"material-icons\">search</i></button>";
-				echo "<button type=\"button\" name=\"search_reset\" onclick=\"XYO.Application.resetSearch();return false;\"><i class=\"material-icons\">close</i></button>";
-				echo "</div>";
-			echo "</form>";
+		if($modApp->hasSearch()) {
+			echo "<div class=\"xui\" id=\"application_search_super\">";
+				echo "<form name=\"application_search\" id=\"application_search_form\">";
+					echo "<div class=\"xui form-input-group\">";
+					echo "<input type=\"text\" name=\"search\" value=\"\" size=\"32\" placeholder=\"Search\" id=\"application_search\">";
+					echo "<button type=\"submit\" name=\"submit_search\" onclick=\"XYO.Application.doSearch();return false;\"><i class=\"material-icons\">search</i></button>";
+					echo "<button type=\"button\" name=\"search_reset\" onclick=\"XYO.Application.resetSearch();return false;\"><i class=\"material-icons\">close</i></button>";
+					echo "</div>";
+				echo "</form>";
+			echo "</div>";
 		};
 	};
 };
