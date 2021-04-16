7b2adf574bab20bae533dfd4e925cafa0c923e4f
Grigore Stefan <g_stefan@yahoo.com>
2021-04-16 10:03:47 +0300

    update

--- build/build.config.cmd
+++ build/build.config.cmd
@@ -4,7 +4,7 @@ rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 set PROJECT=xyo-cloud
-set VERSION_LAST=1.5.0
+set VERSION_LAST=2.0.0
 
 set VENDOR_XUI_VERSION=2.2.0
 
